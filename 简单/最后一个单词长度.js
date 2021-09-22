d77ef5009aa00f6cfc0c5a3e57ed39168e3c3985
Ruler <8439518+lyl-code@user.noreply.gitee.com>
2021-09-22 09:10:11 +0800

    tt

---
+++ 简单/最后一个单词长度.js
@@ -0,0 +1,8 @@
+var lengthOfLastWord = function (s) {
+	let arr = s.split(' ')
+	arr = arr.filter((i) => i !== '')
+	if (arr.length > 0) return arr[arr.length - 1].length
+	else return 0
+}
+
+// lengthOfLastWord("   fly me   to   the moon  ")
