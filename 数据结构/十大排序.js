044e1d95578a778d4f034385039c08e68b805082
lruler <1204312199@qq.com>
2021-09-26 17:22:34 +0800

    shellsort

--- 数据结构/十大排序.js
+++ 数据结构/十大排序.js
@@ -121,5 +121,23 @@ const quick = (array, left, right) => {
     return array
 }
 
-
-console.log(quickSort([1,2,7,5,4,3,12]));
+// 希尔排序 nlogn 不稳定
+例
+function shellSort(arr) {
+    var len = arr.length,
+        temp,
+        gap = 1;
+    while(gap < len/3) {          //动态定义间隔序列
+        gap =gap*3+1;
+    }
+    for (gap; gap > 0; gap = Math.floor(gap/3)) {
+        for (var i = gap; i < len; i++) {
+            temp = arr[i];
+            for (var j = i-gap; j >= 0 && arr[j] > temp; j-=gap) {
+                arr[j+gap] = arr[j];
+            }
+            arr[j+gap] = temp;
+        }
+    }
+    return arr;
+}
