555d9b194924df09b7fc0f6ca1a4b9dad52ec6a9
lruler <1204312199@qq.com>
2021-09-27 10:42:50 +0800

    counting Sort

--- 数据结构/十大排序.js
+++ 数据结构/十大排序.js
@@ -143,7 +143,6 @@ function shellSort(arr) {
 
 
 // 堆排序 nlogn 稳定
-
 function heapSort(arr) {
     let len
 
@@ -184,4 +183,29 @@ function heapSort(arr) {
     return arr
 }
 
-console.log(heapSort([3,7,4,2,1,9,87,123,22,4,5]));
+// 计数排序 n + k 稳定
+
+function countingSort(arr, maxValue) {
+    var bucket = new Array(maxValue+1),
+        sortedIndex = 0;
+        arrLen = arr.length,
+        bucketLen = maxValue + 1;
+
+    for (var i = 0; i < arrLen; i++) {
+        if (!bucket[arr[i]]) {
+            bucket[arr[i]] = 0;
+        }
+        bucket[arr[i]]++;
+    }
+
+    for (var j = 0; j < bucketLen; j++) {
+        while(bucket[j] > 0) {
+            arr[sortedIndex++] = j;
+            bucket[j]--;
+        }
+    }
+
+    return arr;
+}
+
+console.log(countingSort([7,2,9,8,11,52,2,123], 123));
