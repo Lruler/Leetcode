ae93fd08533e30925114886d8491b965315b1b20
lruler <1204312199@qq.com>
2021-09-29 17:24:29 +0800

    add queue

--- 数据结构/队列.js
+++ 数据结构/队列.js
@@ -6,7 +6,45 @@ class Queue {
         this.items = {}
     }
 
+    // 向队列尾部添加新元素
     enqueue(e) {
-        
+        this.items[this.count] = e
+        this.count++
+    }
+    // 移除队列尾部元素
+    dequeue() {
+        if(this.isEmpty()) return undefined
+
+        const result = this.items[this.lowestCount]
+        delete this.items[this.lowestCount]
+        this.lowestCount++
+        return result
+    }
+
+    peek() {
+        if(this.isEmpty()) return undefined
+
+        return this.items[this.lowestCount]
+    }
+
+    isEmpty() {
+        return this.count - this.lowestCount === 0
+    }
+
+    clear() {
+        this.items = {}
+        this.count = 0
+        this.lowestCount = 0
+    }
+
+    toString() {
+        if (this.isEmpty()) return ''
+
+        let objString = `${this.items[this.lowestCount]}`
+        for (let i = this.lowestCount + 1; i < this.count; i++) {
+            objString = `${objString}, ${this.items[i]}`
+        }
+
+        return objString
     }
 }
