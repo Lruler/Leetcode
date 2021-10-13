67b01935dc1f24b8403c3b49a5c5bd66482f5487
lruler <1204312199@qq.com>
2021-10-13 18:11:44 +0800

    final HeadLink list

--- 数据结构/链性表.js
+++ 数据结构/链性表.js
@@ -10,7 +10,7 @@ class Node {
         this.prev = null
     }
 }
-// 定义单向链表
+// 定义单向链表 不带头节点
 class LinkList {
     constructor(equalsFn = defaultEquals) {
         this.size = 0;  // 单链表的长度
@@ -25,7 +25,7 @@ class LinkList {
     // 查找元素
     find (item) {
         let currentNode = this.head;
-        while(currentNode && (currentNode !== item)){
+        while(currentNode && (currentNode.element !== item)){
             currentNode = currentNode.next
         }
         return currentNode
@@ -59,10 +59,11 @@ class LinkList {
             if (index === 0) {
                 const current = this.head
                 node.next = current
+                this.head = node
             }
             else {
                 const pre =  this.getElementAt (index - 1)
-                const current = previous.next
+                const current = pre.next
                 node.next = current
                 pre.next = node
             }
@@ -125,7 +126,7 @@ class LinkList {
         }
         let objString = `${this.head.element}`
         let current = this.head.next
-        for (let i = 0; i < this.size() && current != null; i++) {
+        for (let i = 0; i < this.size && current != null; i++) {
             objString = `${objString},${current.element}`
             current = current.next
         }
@@ -142,14 +143,137 @@ class DoublyLinkList extends LinkList {
     //插入 分三种情况 头部 尾部 中间任意部分
     //移除 一样的三种情况
 }
-
 // 定义循环链表 就是最后一个节点的尾指针指向头节点
 class CircularLinkList extends LinkList {
     constructor (equalsFn = defaultEquals) {
         super (equalsFn)
     }
 }
-
 // 定义有序链表。。。
 
-// 可以用双向链表定义栈
+// 定义带头节点的链表
+class HeadLinkList {
+    constructor(equalsFn = defaultEquals) {
+        this.size = 0; // 单链表的长度
+        this.currNode = ''; // 当前节点的指向
+        this.head = new Node('null'); // 头节点
+        this.equalsFn = equalsFn
+    }
+    // 返回链表长度
+    size() {
+        return this.size
+    }
+    // 查找元素
+    find(item) {
+        let currentNode = this.head;
+        while (currentNode && (currentNode.element !== item)) {
+            currentNode = currentNode.next
+        }
+        return currentNode
+    }
+    // 根据索引查找元素
+    getElementAt(index) {
+        if (index >= 0 && index <= this.size) {
+            let node = this.head
+            for (let i = 0; i < index && node != null; i++) {
+                node = node.next
+            }
+            return node
+        }
+        return undefined
+    }
+    // 返回元素的索引
+    indexOf(element) {
+        let current = this.head
+        for (let i = 0; i < this.size && current != null; i++) {
+            if (this.equalsFn(element, current.element)) {
+                return i
+            }
+            current = current.next
+        }
+        return -1
+    }
+    // 插入元素
+    insert(element, index) {
+        if (index >= 0 && index <= this.size) {
+            const node = new Node(element)
+            if (index === 0) {
+                this.head.next = node
+            } else {
+                const pre = this.getElementAt(index - 1)
+                const current = pre.next
+                node.next = current
+                pre.next = node
+            }
+            this.size++
+            return true
+        }
+        return false
+    }
+    // 移除元素 按索引
+    removeAt(index) {
+        if (index === 0) {
+            this.head = current.next
+        } else {
+            const pre = this.getElementAt(index - 1)
+            pre.next = current.next
+        }
+        this.size--
+        return current.element
+    }
+    // 移除元素 按值
+    remove(element) {
+        const index = this.indexOf(element)
+        return this.removeAt(index)
+    }
+    // 添加元素
+    push(element) {
+        const node = new Node(element)
+        let current
+            current = this.head
+            while (current.next != null)
+                current = current.next
+            current.next = node
+        this.size++
+    }
+    // 查找最后一个元素
+    findLast() {
+        let currentNode = this.head
+
+        while (currentNode.next)
+            currentNode = currentNode.next
+
+        return currentNode
+    }
+    // 判断链表是否为空
+    isEmpty() {
+        return this.size === 0
+    }
+    // 获取头节点
+    getHead() {
+        return this.head
+    }
+    // 把链表转换成一个字符串
+    toString() {
+        let objString = `${this.head.element}`
+        let current = this.head.next
+        for (let i = 0; i < this.size && current != null; i++) {
+            objString = `${objString},${current.element}`
+            current = current.next
+        }
+        return objString
+    }
+}
+
+
+
+
+let list = new HeadLinkList()
+
+list.insert('zk', 0)
+list.insert('ljz', 1)
+list.insert('txn', 2)
+list.insert('zyn', 3)
+list.push('wff')
+
+console.log(list.toString());
