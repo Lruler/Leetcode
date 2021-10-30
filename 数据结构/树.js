aa0bcdec465d2e72976e35350938986925375b9d
lruler <1204312199@qq.com>
2021-10-30 18:55:41 +0800

    add tree node

--- 数据结构/树.js
+++ 数据结构/树.js
@@ -1,10 +1,33 @@
-class Tree {
-    
-
-
-
-
-    
-
-
+const defaultEquals = (a, b) => {
+    return a === b
+}
+class Node {
+    constructor(key) {
+        this.key = key
+        this.left = null
+        this.right = null
+    }
+}
+class BinarySearchTree {
+    constructor(equalsFn = defaultEquals) {
+        this.root = null
+        this.equalsFn = equalsFn
+    }
+    // 插入一个节点
+    insert(key) {
+        if (this.root == null) {
+            this.root = new Node(key)
+        } else {
+            this.insertNode(this.root, key)
+        }
+    }
+    inserNode(node, key) {
+        if (key <= node.key) {
+            if (node.left = null) node.left = new Node(key)
+            else this.insertNode(node.left, key)
+        } else {
+            if (node.right == null) node.right = new Node(key)
+            else this.inserNode(node.right, key)
+        }
+    }
 }
