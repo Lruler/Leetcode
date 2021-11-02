20167379e0daca19bb836cbe636dfd514a193511
lruler <1204312199@qq.com>
2021-11-02 16:45:26 +0800

    12

--- 数据结构/试验题/循环队列打印杨辉三角.js
+++ 数据结构/试验题/循环队列打印杨辉三角.js
@@ -1,5 +1,5 @@
 // 定义循环队列的类
-class MyCircularQueue {
+class CircularQueue {
     constructor(k) {
         this.list = Array(k) // 创建一个长度为k的空数组
         this.front = 0 // 保存头部指针位置
@@ -56,6 +56,46 @@ class MyCircularQueue {
 // 打印杨辉三角的函数
 
 const logYangHui = (row) => {
-        
-
+    let yang = new CircularQueue(row + 2)
+    let i, s, e, k
+    console.log(`1`);
+    yang.enQueue(0)
+    yang.enQueue(1)
+    yang.enQueue(1)
+    k = 1
+        while (k < row) {
+            // //每往下一行，其第一个数字都需往左移动 1 个占位
+            // for (i = 1; i <= row - k; i++) {
+            //     console.log("   ");
+            // }
+            // 0 作为转行符，入队列
+            yang.enQueue(0)
+            do {
+                //队头元素出队列
+                s = yang.Front()
+                yang.deQueue()
+                //取新的队头元素
+                e = yang.Front()
+                //如果所取元素非 0，则输出，否则做转行操作
+                console.log(e,'e是啥')
+                if (e) {
+                    console.log(e);
+                } else {
+                    console.log("\n");
+                }
+                yang.enQueue(s + e)
+            } while (e != 0); //一旦 e 值为 0，即做转行操作，退出循环，开始新一行的排列
+            k++;
+        }
+        e = yang.Front()
+        yang.deQueue()
+        while (!yang.isEmpty()) {
+            e = yang.Front()
+            yang.deQueue()
+            console.log(e, '这里有吗');
+        }
 }
+
+
+
+logYangHui(5)
