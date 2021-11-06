10f01a337fd91dedfded49c9c8a0bdeca742edb8
lruler <1204312199@qq.com>
2021-11-06 13:12:39 +0800

    1

--- 数据结构/树.js
+++ 数据结构/树.js
@@ -5,6 +5,7 @@ class Node {
         this.right = null
     }
 }
+// 二叉树
 class BinarySearchTree {
     constructor() {
         this.root = null
@@ -131,22 +132,8 @@ class BinarySearchTree {
         } 
     }
 }
+// 自平衡树
 
-let tree = new BinarySearchTree()
-tree.insert(11)
-tree.insert(7)
-tree.insert(15)
-tree.insert(5)
-tree.insert(3)
-tree.insert(9)
-tree.insert(8)
-tree.insert(10)
-tree.insert(13)
-tree.insert(12)
-tree.insert(14)
-tree.insert(20)
-tree.insert(18)
-tree.insert(25)
 
 // const printNode = (value) => console.log(value)
 // tree.preOrderTraverse(printNode)
