b6691b9909ee812e18317c04d13f9e7ea32dca9d
lruler <1204312199@qq.com>
2021-11-14 13:21:50 +0800

    二分查找

---
+++ 算系统学习吧/二分查找/69.js
@@ -0,0 +1,29 @@
+/* 
+题目描述
+给定一个非负整数， 求它的开方， 向下取整。
+4.3 查找区间 输入输出样例
+输入一个整数， 输出一个整数。
+Input: 8
+Output: 2
+*/
+
+// 二分查找的方法
+
+const sqrt = (a) => {
+    if (a == 0) return a 
+    let l = 1, r = a, mid, sqrt
+    while (l <= r) {
+        mid = l + (r - l) / 2;
+        sqrt = a / mid;
+        if (sqrt == mid) {
+            return mid;
+        } else if (mid > sqrt) {
+            r = mid - 1;
+        } else {
+            l = mid + 1;
+        }
+    }
+    return r
+}
+
+console.log(sqrt(4))
